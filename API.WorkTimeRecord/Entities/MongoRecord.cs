587ab2b50630993ea4a008d154b6e259a35102db
Ramón Serrano Valero <ramon.serrano.valero@gmail.com>
2024-06-03 23:43:04 +0200

    1 solo

---
+++ API.WorkTimeRecord/Entities/MongoRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace API.WorkTimeRecord.Entities
+{
+    public class MongoRecord
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LastRecord { get; set; }
+        public string LastMode { get; set; }
+    }
+}
